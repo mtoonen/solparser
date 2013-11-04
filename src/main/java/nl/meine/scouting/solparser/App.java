7e7d8656b93af87ac0d90341ed389e359540852a
Meine Toonen <toonen.meine@gmail.com>
2013-11-04 20:29:55 +0100

    Initial import

---
+++ src/main/java/nl/meine/scouting/solparser/App.java
@@ -0,0 +1,13 @@
+package nl.meine.scouting.solparser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/nl/meine/scouting/solparser/Parser.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.meine.scouting.solparser;
+
+/**
+ *
+ * @author meine
+ */
+public class Parser {
+    
+}
