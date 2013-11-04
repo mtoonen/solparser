a89cc5040cb948784146ad9849b3a9574f5f7f4d
Meine Toonen <toonen.meine@gmail.com>
2013-11-04 21:44:46 +0100

    Reading of personline updated.

--- src/main/java/nl/meine/scouting/solparser/Parser.java
+++ src/main/java/nl/meine/scouting/solparser/Parser.java
@@ -20,7 +20,7 @@ import org.apache.poi.ss.usermodel.Sheet;
 public class Parser {
 
     public static void main(String[] args) {
-        Parser p = new Parser("selectie_2871.csv", "aap.xls");
+        Parser p = new Parser("test.csv", "aap.xls");
         p.read();
     }
     private File input;
@@ -50,6 +50,7 @@ public class Parser {
                 str = str.replaceAll("\"", "");
                 String [] ar = str.split(";");
                 Person p = createPerson(ar);
+                sortedPersonsPerSpelenheid.put(p.getSpeleenheid(), p);
             }
             in.close();
         } catch (IOException e) {
@@ -65,32 +66,33 @@ public class Parser {
         p.setLid_initialen(row[2]);
         p.setLid_tussenvoegsel(row[3]);
         p.setLid_achternaam(row[4]);
-        p.setLid_huisnummer(row[5]);
-        p.setLid_toevoegsel_huisnr(row[6]);
-        p.setLid_postcode(row[7]);
+        p.setLid_straat(row[5]);
+        p.setLid_huisnummer(row[6]);
+        p.setLid_toevoegsel_huisnr(row[7]);
+        p.setLid_postcode(row[8]);
 //"lid postcode";"lid plaats";"lid land";"lid mailadres";"Lid mailadres ouder/verzorger";"Lid geslacht";"lid geboortedatum";"lid mobiel";
-        p.setLid_plaats(row[8]);
-        p.setLid_land (row[9]);
-        p.setLid_mailadres(row[10]);
-        p.setLid_mailadres_ouder_verzorger (row[11]);
-        p.setLid_geslacht(row[12]);
-        p.setLid_geboortedatum(row[13]);
-        p.setLid_mobiel(row[14]);
+        p.setLid_plaats(row[9]);
+        p.setLid_land (row[10]);
+        p.setLid_mailadres(row[11]);
+        p.setLid_mailadres_ouder_verzorger (row[12]);
+        p.setLid_geslacht(row[13]);
+        p.setLid_geboortedatum(row[14]);
+        p.setLid_mobiel(row[15]);
 //"lid telefoon";"functie";"functie startdatum";"Functie status";"Functienummer";"Functietype";"speleenheid soort";"speleenheid";"Speleenheidnummer"
-        p.setLid_telefoon(row[15]);
-        p.setFunctie(row[16]);
-        p.setFunctie_startdatum(row[17]);
-        p.setFunctie_status(row[18]);
-        p.setFunctienummer(row[19]);
-        p.setFunctietype(row[20]);
-        p.setSpeleenheid_soort(row[21]);
-        p.setSpeleenheid(row[22]);
-        p.setSpeleenheidnummer(row[23]);
+        p.setLid_telefoon(row[16]);
+        p.setFunctie(row[17]);
+        p.setFunctie_startdatum(row[18]);
+        p.setFunctie_status(row[19]);
+        p.setFunctienummer(row[20]);
+        p.setFunctietype(row[21]);
+        p.setSpeleenheid_soort(row[22]);
+        p.setSpeleenheid(row[23]);
+        p.setSpeleenheidnummer(row[24]);
 //;"organisatienummer";"organisatie categorie";"organisatie";"organisatie plaats"
-        p.setOrganisatienummer(row[24]);
-        p.setOrganisatie_categorie(row[25]);
-        p.setOrganisatie(row[26]);
-        p.setOrganisatie_plaats(row[27]);
+        p.setOrganisatienummer(row[25]);
+        p.setOrganisatie_categorie(row[26]);
+        p.setOrganisatie(row[27]);
+        p.setOrganisatie_plaats(row[28]);
         return p;
     }
 
