d799904a1ff13079c3c40754c116a584e54cb26c
Meine Toonen <toonen.meine@gmail.com>
2013-11-06 08:37:04 +0100

    Added writer interface and beginning of excelwriter. Added license headers

---
+++ src/main/java/nl/meine/scouting/solparser/writer/ExcelWriter.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2013 Meine Toonen
+ * This file is part of the SolParser library.
+ *
+ *  The SolParser librar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The SolParser librar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the SolParser librar. If not, see <http://www.gnu.org/licenses/>.
+ */
+package nl.meine.scouting.solparser.writer;
+
+/**
+ *
+ * @author Meine Toonen
+ */
+public class ExcelWriter {
+    
+}
