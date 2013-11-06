a8bd342acd27abfe843e7b8066345abcae388175
Meine Toonen <toonen.meine@gmail.com>
2013-11-06 09:08:34 +0100

    License header spacing correctly (and added for main class)

--- src/main/java/nl/meine/scouting/solparser/Parser.java
+++ src/main/java/nl/meine/scouting/solparser/Parser.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2013 Meine Toonen
- * This file is part of the SolParser library.
+ *  Copyright (C) 2013 Meine Toonen
+ *  This file is part of the SolParser library.
  *
  *  The SolParser librar is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
