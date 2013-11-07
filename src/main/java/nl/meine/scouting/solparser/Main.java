a67e224633a73dfc1a6f578d64cafdf57122a157
Meine Toonen <toonen.meine@gmail.com>
2013-11-07 10:06:25 +0100

    Added helptext

--- src/main/java/nl/meine/scouting/solparser/Main.java
+++ src/main/java/nl/meine/scouting/solparser/Main.java
@@ -31,12 +31,23 @@ import nl.meine.scouting.solparser.writer.SolWriter;
  * @author Meine Toonen
  */
 public class Main {
-
+    public static final String HELP_TEXT = "SolParser Library created by Meine Toonen. Version 0.5. \n"
+            + "\t Use the SolParser library to convert a horrifying csv file to a more human readable excel file, with handy grouping options. The following parameters are permitted: \n"
+            + "\t\t Usage: \n"
+            + "\t\t\t-i \t Required: Name to input file. \n"
+            + "\t\t\t-o \t Required: Name of the file to output. \n"
+            + "\t\t\t-s \t Optional (default is unit): Sorting options: which tabs should be created. Available options: leaders, bestuur, leadersandbestuur, unit \n"
+            + "\t\t\t-ot \t Optional (default is excel): Which output should be generated. Available options: excel \n"
+            + "\t\t\t-h \t Display this helptext. \n"
+            + "\t\t\t-sf \t Optional (default true) Skip the first line of the csv value.";
     public static void main(String[] args) throws Throwable {
 
         Properties prop = argsParser(args);
-        if (prop.contains("action")) {
+        if (prop.containsKey("action")) {
+            
+            System.out.println(HELP_TEXT);
         } else {
+            //-s bestuur -ot excel -sf true -i selectie_2871.csv -o aap.xls
             Sorter sorter = getSorter(prop.getProperty("sorter"));
             SolWriter writer = getWriter(prop.getProperty("outputtype"));
             boolean skipfirst = prop.getProperty("skipfirst") == null ? true : Boolean.parseBoolean(prop.getProperty("skipfirst"));
@@ -58,19 +69,21 @@ public class Main {
         Properties prop = new Properties();
         for (int i = 0; i < args.length; i += 2) {
             String key = args[i];
-            String value = args[i + 1];
-            if (key.equalsIgnoreCase("-s")) {
-                prop.setProperty("sorter", value);
-            } else if (key.equalsIgnoreCase("-ot")) {
-                prop.setProperty("outputtype", value);
-            } else if (key.equalsIgnoreCase("-sf")) {
-                prop.setProperty("skipfirst", value);
-            } else if (key.equalsIgnoreCase("-h")) {
+            if (key.equalsIgnoreCase("-h")) {
                 prop.setProperty("action", "help");
-            } else if (key.equalsIgnoreCase("-i")) {
-                prop.setProperty("input", value);
-            } else if (key.equalsIgnoreCase("-o")) {
-                prop.setProperty("output", value);
+            } else {
+                String value = args[i + 1];
+                if (key.equalsIgnoreCase("-s")) {
+                    prop.setProperty("sorter", value);
+                } else if (key.equalsIgnoreCase("-ot")) {
+                    prop.setProperty("outputtype", value);
+                } else if (key.equalsIgnoreCase("-sf")) {
+                    prop.setProperty("skipfirst", value);
+                } else if (key.equalsIgnoreCase("-i")) {
+                    prop.setProperty("input", value);
+                } else if (key.equalsIgnoreCase("-o")) {
+                    prop.setProperty("output", value);
+                }
             }
         }
         return prop;
