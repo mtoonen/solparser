de1cd4e4ad4cca52c3a554a6057ce7fafc56a097
Meine Toonen <toonen.meine@gmail.com>
2014-10-13 22:26:52 +0200

    Corrected constant and added test

---
+++ src/test/java/nl/meine/scouting/solparser/sorter/BestuurSorterTest.java
@@ -0,0 +1,65 @@
+/*
+ *  Copyright (C) 2013-2014 Meine Toonen
+ *  This file is part of the SolParser library.
+ *
+ *  The SolParser librar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The SolParser librar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with the SolParser librar. If not, see <http://www.gnu.org/licenses/>.
+ */
+package nl.meine.scouting.solparser.sorter;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import java.util.List;
+import java.util.Map;
+import nl.meine.scouting.solparser.Parser;
+import nl.meine.scouting.solparser.ParserTest;
+import nl.meine.scouting.solparser.entities.Person;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author meine
+ */
+public class BestuurSorterTest {
+    Parser parser;
+    public BestuurSorterTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of sort method, of class BestuurSorter.
+     */
+    @Test
+    public void testSort() throws URISyntaxException {
+        File twopersons = ParserTest.getResource("twopersons.csv");
+        parser = new Parser(twopersons, SorterFactory.createSorter(SorterFactory.BESTUUR_SORTER));
+        parser.read(true);
+        assertEquals(2, parser.getAllPersons().size());
+        
+        Map<String,List<Person>> sorted = parser.getSortedPersons();
+        assertEquals(2, sorted.keySet().size());
+        assertTrue(sorted.containsKey("Besturen"));
+        assertEquals(1, sorted.get("Besturen").size());
+    }
+    
+}
